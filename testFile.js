3c495250ec9c3a10c4436cf68148080021483c39
Sapana Kale <44019343+sapanaKale@users.noreply.github.com>
2018-11-20 12:44:22 +0530

    Some more test cases are added.

--- testFile.js
+++ testFile.js
@@ -7,6 +7,10 @@ const increment = function (number) {
   return number + 1;
 }
 
+const square = function (number) {
+  return number * number;
+}
+
 //testing map function.
 
 const testMap = function (funcRef,source,expectedOutput) {
@@ -15,5 +19,8 @@ const testMap = function (funcRef,source,expectedOutput) {
 }
 
 testMap(increment,[1],[2]);
+testMap(increment,[1,2,3],[2,3,4]);
+testMap(square,[1],[1]);
+testMap(square,[1,2,3],[1,4,9])
 
 console.log("all tests are passed");
